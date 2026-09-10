c9f6926342602cb433f40d2510d3a30a53ef868a
saroj jena <jenas8290@gmail.com>
2026-09-10 12:57:33 +0530

    Set basename for BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
     <MyContext>
 
 
-      <BrowserRouter>
+    <BrowserRouter basename="/Tech-shop-React-js">
         <Provider store={store}>
           <App />
         </Provider >
